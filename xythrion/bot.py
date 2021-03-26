55aebe5c2b916189c2810b2b5cdc6c7c74c17061
Xithrius <Xithrius@gmail.com>
2021-03-26 12:19:57 -0700

    Moved connection initialization into login method.

--- xythrion/bot.py
+++ xythrion/bot.py
@@ -1,10 +1,13 @@
 import asyncio
 import logging
 from datetime import datetime
+from typing import Optional
 
 import aiohttp
+import asyncpg
 from discord.ext.commands import Bot
 
+from xythrion.constants import Postgresql
 from xythrion.databasing import Database
 
 log = logging.getLogger(__name__)
@@ -17,28 +20,34 @@ class Xythrion(Bot):
         """Creating import attributes."""
         super().__init__(*args, **kwargs)
 
-        # Setting the loop.
-        self.loop = asyncio.get_event_loop()
+        self.pool: Optional[asyncpg.pool.Pool] = None
+        self.http_session: Optional[aiohttp.ClientSession] = None
 
-        # Creating session for web requests.
-        self.http_session = aiohttp.ClientSession()
-
-        # Setting when the bot started up.
-        self.startup_time = datetime.now()
-
-        # Setting up the database.
-        self.database = Database(self.loop)
-        self.pool = self.database.pool
+        self.database = Database(self.pool)
 
     @staticmethod
     async def on_ready() -> None:
         """Updates the bot status when logged in successfully."""
         log.trace("Awaiting...")
 
+    async def login(self, *args, **kwargs) -> None:
+        """Creating all the important connections."""
+        try:
+            self.pool = asyncpg.create_pool(Postgresql.asyncpg_config_url, command_timeout=60, loop=self.loop)
+
+            log.trace("Successfully connected to Postgres database.")
+
+        except Exception as e:
+            log.error("Failed to connect to Postgresql database", exc_info=(type(e), e, e.__traceback__))
+
+        self.http_session = aiohttp.ClientSession()
+
+        await super().login(*args, **kwargs)
+
     async def logout(self) -> None:
         """Subclassing the logout command to ensure connection(s) are closed properly."""
-        await asyncio.wait_for(self.http_session.close(), 30.0, loop=self.loop)
+        await asyncio.wait(fs={self.pool.close(), self.http_session.close()}, loop=self.loop, timeout=30.0)
 
-        log.trace("Finished up closing task(s).")
+        log.trace("Finished closing task(s).")
 
         return await super().logout()
