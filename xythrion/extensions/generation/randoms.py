bcd176cc92dad630acf46390cb0986d542763b4d
Xithrius <xithrius@gmail.com>
2021-04-04 03:22:49 -0700

    Preparing randoms for random graphs.

--- xythrion/extensions/generation/randoms.py
+++ xythrion/extensions/generation/randoms.py
@@ -1,9 +1,9 @@
-from random import choice, sample
+import random
 
 import numpy as np
 from discord.ext.commands import Cog, Context, command
 
-from xythrion.bot import Xythrion
+from xythrion import Xythrion
 from xythrion.utils import DefaultEmbed
 
 
@@ -17,7 +17,7 @@ class Randoms(Cog):
     async def dice(self, ctx: Context, rolls: int = 1) -> None:
         """Rolls a die anywhere between 1 and 100."""
         if 1 < rolls < 100:
-            s = round(np.sum(sample(range(1, 6), rolls)) / rolls, 3)
+            s = round(np.sum(random.sample(range(1, 6), rolls)) / rolls, 3)
             msg = f"Die was rolled {rolls} time(s). Average output: {s}"
         else:
             msg = "Integer gives for rolls is invalid."
@@ -26,8 +26,8 @@ class Randoms(Cog):
 
         await ctx.send(embed=embed)
 
-    @command(aliases=("pick",))
+    @command(aliases=("pick", "choice"))
     async def choose(self, ctx: Context, *choices) -> None:
         """Returns only one of the items that the user gives."""
-        embed = DefaultEmbed(ctx, description=f"A choice was made. Fate landed on {choice(choices)}.")
+        embed = DefaultEmbed(ctx, description=f"A choice was made. Fate landed on {random.choice(choices)}.")
         await ctx.send(embed=embed)
