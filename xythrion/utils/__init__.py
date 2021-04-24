bafba250abe6d4329cec8f790774285982fd4f1b
Xithrius <Xithrius@gmail.com>
2021-04-24 06:18:56 -0700

    Moved DefaultEmbed into xythrion/context.py

--- xythrion/utils/__init__.py
+++ xythrion/utils/__init__.py
@@ -1,12 +1,10 @@
 from .converters import Extension, remove_whitespace
+from .formatting import and_join, markdown_link
 from .graphs import graph_2d
-from .shortcuts import DefaultEmbed, and_join, check_for_subcommands, markdown_link
 
 __all__ = (
     "graph_2d",
     "and_join",
-    "DefaultEmbed",
-    "check_for_subcommands",
     "markdown_link",
     "remove_whitespace",
     "Extension",
---
+++ xythrion/utils/formatting.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+
+def markdown_link(s: str, link: str) -> str:
+    """Gets rid of the thinking while creating a link for markdown."""
+    return f"[`{s}`]({link})"
+
+
+def and_join(items: Union[list, tuple], sep: str = ", ") -> str:
+    """Joins a list by a separator with an 'and' at the very end for readability."""
+    return f"{sep.join(str(x) for x in items[:-1])}{sep}and {items[-1]}"
